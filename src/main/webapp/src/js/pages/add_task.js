09b3830c95a024aa9318d6acfcf515104eddd646
Alex <alryumin@gmail.com>
2017-11-24 21:58:18 +0200

    Add task tinymce adjustments.

--- src/main/webapp/src/js/pages/add_task.js
+++ src/main/webapp/src/js/pages/add_task.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
     height: 100,
     menubar: false,
     plugins: [
-      'advlist autolink lists link image charmap print preview anchor textcolor',
+      'advlist autolink lists link image charmap print preview  textcolor',
       'searchreplace visualblocks code fullscreen',
-      'insertdatetime media table contextmenu paste code help'
+      'insertdatetime media table contextmenu paste code'
     ],
-    toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+    toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat',
   });
 });
